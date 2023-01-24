ad2149440a8ad23737049d878460653a68bfdb47
Spencer <smaroukis@gmail.com>
2023-01-24 12:35:34 +0900

    wiped

--- _build/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -v $(pwd):/srv/jekyll jekyll/jekyll:3.8 jekyll build
--- _build/serve.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -p 4000:4000 -v $(pwd):/srv/jekyll jekyll/jekyll:3.8 jekyll serve --watch
